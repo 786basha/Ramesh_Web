bc07356997a6704face915f8cc4b278a16bb9138
786basha <bashamasthan31@gmail.com>
2023-03-31 23:12:32 +0530

    0o0oss

---
+++ java.js
@@ -0,0 +1,9 @@
+function authh(){
+    var usr = document.getElementById('usr_n').value;
+    var pas = document.getElementById('pass').value;
+    if (usr == "admin"){
+        if(pas == "password"){
+            
+        }
+    }
+}
