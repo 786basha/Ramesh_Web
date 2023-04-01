f8150fab5d5ef8f630e75462ecfd3307d4ebd584
786basha <bashamasthan31@gmail.com>
2023-04-01 23:41:33 +0530

    0o0os

--- java.js
+++ java.js
@@ -3,7 +3,14 @@ function authh(){
     var pas = document.getElementById('pass').value;
     if (usr == "admin"){
         if(pas == "password"){
-            
+            window.location.href = "http://34.222.33.233/Admin_ID=123/";
+            alert( "Login as " + usr);
         }
+        else{
+            alert("Invalid password !");
+        }
+    }
+    else{
+        alert("Spoof detected !")
     }
 }
